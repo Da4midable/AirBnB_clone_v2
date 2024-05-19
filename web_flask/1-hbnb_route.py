d3d30b2ed0679a8d0e254c9582aba0e1f90a878f
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-05-19 19:25:39 +0100

    add /hbnb

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This module starts a Flask web application.
+
+The web application listens on 0.0.0.0, port 5000.
+Routes:
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+The route definitions use the option strict_slashes=False.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    Displays a simple greeting.
+
+    This function handles the root route and returns the string
+    "Hello HBNB!" when accessed.
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Displays a simple message.
+
+    This function handles the /hbnb route and returns the string
+    "HBNB" when accessed.
+    """
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
