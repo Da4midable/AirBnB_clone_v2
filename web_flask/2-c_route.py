b94a9328b4c2aac9a782453354772fce3675285b
Saheed Kehinde Yusuf <saheed.k.yusuf@gmail.com>
2024-05-19 19:43:14 +0100

    C flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,6 +6,8 @@ The web application listens on 0.0.0.0, port 5000.
 Routes:
 /: display “Hello HBNB!”
 /hbnb: display “HBNB”
+/c/<text>: display “C ” followed by the value of the text variable
+(replace underscore _ symbols with a space)
 The route definitions use the option strict_slashes=False.
 """
 
@@ -32,6 +34,17 @@ def hbnb():
     "HBNB" when accessed.
     """
     return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Displays a message with a variable.
+
+    This function handles the /c/<text> route and returns the string
+    "C " followed by the value of the text variable, with underscores
+    replaced by spaces.
+    """
+    return "C " + text.replace('_', ' ')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
